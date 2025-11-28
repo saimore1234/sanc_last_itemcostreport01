d1df183acf7d1775b9bfe564913e9b856be2d822
saimore1234 <moresai899@gmail.com>
2025-11-28 23:54:17 +0530

    incomingrate changes

--- sancreports/sancreport/report/last_item_cost/last_item_cost.py
+++ sancreports/sancreport/report/last_item_cost/last_item_cost.py
@@ -1,48 +1,39 @@
 import frappe
 
 def execute(filters=None):
-    data = frappe.db.sql("""
+    columns = [
+        {"label": "Item Code", "fieldname": "item_code", "fieldtype": "Link", "options": "Item", "width": 140},
+        {"label": "Item Name", "fieldname": "item_name", "fieldtype": "Data", "width": 260},
+        {"label": "Latest Incoming Date", "fieldname": "latest_posting_date", "fieldtype": "Date", "width": 130},
+        {"label": "Latest Incoming Rate", "fieldname": "latest_incoming_rate", "fieldtype": "Currency", "width": 150},
+    ]
+
+    data = frappe.db.sql(
+        """
         SELECT
-            i.item_code,
+            i.name AS item_code,
             i.item_name,
-            sle.posting_date AS latest_posting_date,
-            FORMAT(sle.incoming_rate, 2) AS latest_incoming_rate,
-            FORMAT(lci.applicable_charges, 2) AS latest_lcv
+
+            DATE_FORMAT(sle.posting_date, '%d-%m-%Y') AS latest_posting_date,
+            sle.incoming_rate AS latest_incoming_rate
+
         FROM `tabItem` i
+
         LEFT JOIN `tabStock Ledger Entry` sle
             ON sle.name = (
                 SELECT s2.name
                 FROM `tabStock Ledger Entry` s2
-                WHERE s2.item_code = i.item_code
-                  AND s2.incoming_rate > 0
+                WHERE s2.item_code = i.name
+                  AND s2.actual_qty > 0
+                  AND s2.docstatus < 2
+                  AND s2.is_cancelled = 0
                 ORDER BY s2.posting_date DESC, s2.posting_time DESC, s2.creation DESC
                 LIMIT 1
             )
-        LEFT JOIN `tabLanded Cost Item` lci
-            ON lci.item_code = i.item_code
-            AND lci.parent = (
-                SELECT lcv2.name
-                FROM `tabLanded Cost Voucher` lcv2
-                JOIN `tabLanded Cost Item` lci2 ON lci2.parent = lcv2.name
-                WHERE lci2.item_code = i.item_code
-                  AND lcv2.docstatus = 1
-                ORDER BY lcv2.posting_date DESC, lcv2.creation DESC
-                LIMIT 1
-            )
-        ORDER BY i.item_code;
-    """, as_dict=True)
 
-    # 🔥 Format date to dd-mm-yyyy
-    for row in data:
-        if row.latest_posting_date:
-            row.latest_posting_date = row.latest_posting_date.strftime("%d-%m-%Y")
-
-    columns = [
-        {"label": "Item Code", "fieldname": "item_code", "fieldtype": "Data", "width": 160},
-        {"label": "Item Name", "fieldname": "item_name", "fieldtype": "Data", "width": 260},
-        {"label": "Latest Incoming Date", "fieldname": "latest_posting_date", "fieldtype": "Data", "width": 150},
-        {"label": "Latest Incoming Rate", "fieldname": "latest_incoming_rate", "fieldtype": "Currency", "width": 150},
-        {"label": "Latest LCV", "fieldname": "latest_lcv", "fieldtype": "Currency", "width": 150},
-    ]
+        ORDER BY i.name ASC
+        """,
+        as_dict=True,
+    )
 
     return columns, data
